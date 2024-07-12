fe01af1aec43f2e5a0bb1610ff8c37b54e290274
MindVisceral <MindVisceral@protonmail.com>
2024-07-12 12:18:27 +0200

    Towers detect and undetect Enemies

--- Entities/Towers/BaseTower.gd
+++ Entities/Towers/BaseTower.gd
@@ -46,42 +46,43 @@ func _ready() -> void:
 	ShotDelayTimer.wait_time = shot_delay
 
 func _physics_process(delta: float) -> void:
-	## If the current_target isn't null...
-	if current_target:
-		current_target.speed = 20
-	## If there is no current_target, attempt to find one in the Array
-	elif !EnemiesInRangeArray.is_empty():
-		change_current_target()
-
-
-func change_current_target() -> void:
-	## First, check which Enemy this Tower is set to target
-	match enemy_to_target:
-		## If this Tower is set to "First"...
-		0:
-			## Set the current target to the Enemy that is furthest along the track
-			## (The Enemy that was added to the EnemiesInRangeArray earliest)
-			current_target = EnemiesInRangeArray[0]
-		## If this Tower is set to "Last"...
-		1:
-			## Set the current target to the first Enemy within this Tower's range
-			## (The Enemy that was added to the EnemiesInRangeArray latest)
-			current_target = EnemiesInRangeArray.back()
-
+	print(current_target)
 
 ## When TowerDetectionArea sees a new Enemy in range...
 func new_enemy_in_range(Enemy: BaseEnemy) -> void:
 	EnemiesInRangeArray.append(Enemy)
+	
+	print("ENTERED")
+	
+	## A new Enemy has entered the Tower's range.
+	change_current_target()
 
 ## When TowerDetectionArea sees an Enemy is out of range...
 func new_enemy_out_of_range(Enemy: BaseEnemy) -> void:
 	EnemiesInRangeArray.erase(Enemy)
+	
+	print("EXITED")
+	
+	## This Enemy which just went out of range may have been the current_target. If it was,
+	## current_target will be changed
+	change_current_target()
 
 
-
-
-
-
-
-
-
+func change_current_target() -> void:
+	## Forget about the current_target, just in case.
+	current_target = null
+	
+	## First, check if there are any Enemies that can be targetted
+	if !EnemiesInRangeArray.is_empty():
+		## Second, check which Enemy this Tower is set to target
+		match enemy_to_target:
+			## If this Tower is set to "First"...
+			0:
+				## Set the current target to the Enemy that is furthest along the track
+				## (The Enemy that was added to the EnemiesInRangeArray earliest)
+				current_target = EnemiesInRangeArray[0]
+			## If this Tower is set to "Last"...
+			1:
+				## Set the current target to the first Enemy within this Tower's range
+				## (The Enemy that was added to the EnemiesInRangeArray latest)
+				current_target = EnemiesInRangeArray.back()
